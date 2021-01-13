537222ee8998f5ae10e8a9e78cb205606c010749
Hans-Martin Duringhof <hmduringhof@gmail.com>
2021-01-13 12:26:44 +0100

    Implemented FP.5a

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -4,12 +4,22 @@
 
 /* Implementation of class "MessageQueue" */
 
-template <typename T>
-T MessageQueue<T>::receive()
-{
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
-    // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+template <typename T> T MessageQueue<T>::receive() {
+
+  // Done.5a : The method receive should use std::unique_lock<std::mutex> and
+  // _condition.wait() to wait for and receive new messages and pull them from
+  // the queue using move semantics. The received object should then be returned
+  // by the receive function.
+
+  // perform queue modification under the lock
+  std::unique_lock<std::mutex> uLock(_mutex);
+  _cond.wait(uLock, [this] { return !_queue.empty(); }); // pass unique lock to condition variable
+
+  // remove last vector element from queue
+  T msg = std::move(_queue.back());
+  _queue.pop_back();
+
+  return msg;
 }
 
 template <typename T> void MessageQueue<T>::send(T &&msg) {
